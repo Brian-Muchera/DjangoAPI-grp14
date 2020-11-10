617a8ec6f44b5ddd840c5935a4f4df3fdc2c7ca7
CindyKatoni <katmuema@gmail.com>
2020-11-10 15:58:45 +0300

    Create API methods for Doctor

--- group14/views.py
+++ group14/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+
+from group14.models import Doctor
+from group14.serializers import DoctorSerializer
 
 # Create your views here.
