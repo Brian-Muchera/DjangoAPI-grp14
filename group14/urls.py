bb42171cb0f116a8a2a59168019e0371f1391d4b
Brian-Muchera <mucherabrian2@gmail.com>
2020-11-10 13:28:32 +0300

    added a template

--- group14/urls.py
+++ group14/urls.py
@@ -12,10 +12,7 @@ router.register('profile', views.ProfileViewSet)
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('account/', include('django.contrib.auth.urls')),
-    path('api/', include(router.urls)),
+    path('viewset/', include(router.urls)),
     path('<username>/profile', views.user_profile, name='userprofile'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('profile/<username>/', views.profile, name='profile'),
-    path('profile/<username>/settings', views.edit_profile, name='edit'),
-]
+    
