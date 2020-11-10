e8942ab7c2e33c193f9538cddb65ae6ff9025069
Kate <you@example.com>
2020-11-10 12:52:04 +0300

    appointments models, view functions created

---
+++ group14/urls.py
@@ -0,0 +1,15 @@
+from . import views 
+from django.urls import path
+from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
+from group14.views import update_bookings,delete_bookings
+
+
+
+urlpatterns = [
+
+    url(r'^patients/(?P<id>\d+)/$', views.update_bookings, name='update_bookings'),
+    url(r'^patients/(?P<id>\d+)/delete$', views.delete_bookings, name='delete_bookings'),
+
+]
