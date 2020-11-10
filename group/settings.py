8e6540c8da3dc91d3521c4382154d099e3ae1727
CindyKatoni <katmuema@gmail.com>
2020-11-10 15:40:50 +0300

    Create database hosi

--- group/settings.py
+++ group/settings.py
@@ -81,11 +81,12 @@ WSGI_APPLICATION = 'group.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hosi',
+        'USER': 'cindy',
+    'PASSWORD':'group14',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
