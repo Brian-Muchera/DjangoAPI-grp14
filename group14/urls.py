75a00f3330f8b45445ee38cc334551f9dba0c1eb
Brian-Muchera <mucherabrian2@gmail.com>
2020-11-10 14:03:48 +0300

    solved conflicts

--- group14/urls.py
+++ group14/urls.py
@@ -1,26 +1,22 @@
 from django.urls import path, include
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
-from . import views
 from rest_framework import routers
 from .views import ProfileViewSet
-
-router = routers.DefaultRouter()
-router.register('profile', views.ProfileViewSet)
 from . import views 
-from django.urls import path
-from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
 from group14.views import update_bookings,delete_bookings
+
+router = routers.DefaultRouter()
+router.register('profile', views.ProfileViewSet)
 
 
 
 urlpatterns = [
     url(r'^viewset/',include(router.urls)),
     url(r'^$',views.home,name='home'),
-    url(r'^profile/',views.Profile),
-
+    url(r'^profile/(?P<id>\d+)/$', views.Profile, name='prof'),
     url(r'^patients/(?P<id>\d+)/$', views.update_bookings, name='update_bookings'),
     url(r'^patients/(?P<id>\d+)/delete$', views.delete_bookings, name='delete_bookings'),
 
