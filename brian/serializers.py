5ae45cfa18da34284b2ed439f7df927f6054e2ee
Brian-Muchera <mucherabrian2@gmail.com>
2020-11-11 18:59:42 +0300

    made correction

--- brian/serializers.py
+++ brian/serializers.py
@@ -5,4 +5,4 @@ from django.contrib.auth.models import User
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = ['name', 'profile_picture', 'bio', 'location', 'contact']
+        fields = ['id', 'name', 'profile_picture', 'bio', 'location', 'contact']
--- brian/urls.py
+++ brian/urls.py
@@ -1,19 +1,18 @@
 from django.urls import path, include
 from django.conf.urls import url
-from django.contrib.auth import views as auth_views
+#from django.contrib.auth import views as auth_views
 from rest_framework import routers
 from .views import ProfileViewSet
 from . import views 
-from django.conf import settings
-from django.conf.urls.static import static
-from rest_framework import routers
+#from django.conf import settings
+#from django.conf.urls.static import static
+#from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register('profile', views.ProfileViewSet)
+router.register(r'profile', views.ProfileViewSet)
 
 
 urlpatterns = [
-    url(r'^viewset/',include(router.urls)),
-    url('api/', include(router.urls)),
-
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
