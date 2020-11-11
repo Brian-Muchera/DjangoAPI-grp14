46162530723cb342125d628560481622914dee1f
CindyKatoni <katmuema@gmail.com>
2020-11-11 15:07:16 +0300

    GET API Method

--- group14/views.py
+++ group14/views.py
@@ -6,3 +6,9 @@ from group14.models import Doctor
 from group14.serializers import DoctorSerializer
 
 # Create your views here.
+@csrf_exempt
+def doctorApi(request,id=0):
+    if request.method=='GET':
+        doctor = Doctor.objects.all()
+        doctor_serializer = DoctorSerializer(doctor, many=True)
+        return JsonResponse(doctor_serializer.data, safe=False)
