37b867d3cce6b38254c494dab3ebacf79f518365
J-Okoto <okotojr@gmail.com>
2020-11-13 10:09:43 +0300

    added new database

--- group14/urls.py
+++ group14/urls.py
@@ -13,9 +13,9 @@ from group14.views import PatientRegistration,DoctorRegistration,UserLogin
 
 
 urlpatterns=[
-    path('register/patient/', views.PatientRegistration.as_view()),
-    path('register/doctor/', views.DoctorRegistration.as_view()),
-    path('user/',views.UserLogin.as_view()),
+    path('register/patient/', views.PatientRegistration.as_view(),name='register_patient'),
+    path('register/doctor/', views.DoctorRegistration.as_view(),name='register_doctor'),
+    path('user/',views.UserLogin.as_view(),name='user_login'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
