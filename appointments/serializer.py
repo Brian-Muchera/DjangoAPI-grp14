b78507ab919953cd96607aded27b6f70ca3608dc
CindyKatoni <katmuema@gmail.com>
2020-11-25 18:05:22 +0300

    Edited appointments serializers

--- appointments/serializer.py
+++ appointments/serializer.py
@@ -7,4 +7,4 @@ from rest_framework import routers, serializers, viewsets
 class AppointmentsSerializer(serializers.ModelSerializer):
     class Meta:
         model=Appointments
-        fields=('id','patient','doctor', 'date','time_alloted','is_completed','is_confirmed','is_rejected','is_confirmed','created_at','updated_at')
+        fields=('doctor', 'date','time_alloted')
